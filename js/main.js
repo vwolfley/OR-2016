345620fbb9be201663e87175da46b36d8c025354
Vern Wolfley <vwolfley@users.noreply.github.com>
2014-06-05 14:21:34 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,12 +13,12 @@ function initialize() {
   map.data.loadGeoJson(jsonData);
   
   var infoWindow = new google.maps.InfoWindow({
-    content: event.feature.getProperty('Name');
+    content: content
   });
 
   // Set mouseover event for each feature.
   map.data.addListener('mouseover', function(event) {
-    
+    content = event.feature.getProperty('Name');
   });
 
 }
