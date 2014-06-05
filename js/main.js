a395de6121aec12b4a918c4a664e1939e11488a1
Vern Wolfley <vwolfley@users.noreply.github.com>
2014-06-05 16:20:32 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ function initialize() {
     phone = event.feature.getProperty("Phone");
     web = event.feature.getProperty("Website");
     
-    content = name + "</br>" + fam + "</br>" + address + "</br>" + city + "</br>" + phone + "</br>" + web;
+    content = name + "</br>" + address + "</br>" + city + "</br>" + phone + "</br>" + web;
     
     infoWindow.setContent(content);
 
